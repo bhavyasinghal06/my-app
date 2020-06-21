6bcef7e15f2498ef56bc7e5da7b574dc3d865a10
Bhavya Singhal <bhavyasinghal0601@gmail.com>
2020-06-21 13:21:52 +0530

    Updates

---
+++ precache-manifest.94a79245ee38b5ce68658bd28a150cdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e398163a9eba96f2d63a8092b074bef",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143d86b19a24d9a686df",
+    "url": "/my-app/static/js/main.149659bf.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
